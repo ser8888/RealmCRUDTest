ad118a25dfda30572582cd7726cbb3ac3e5c5674
Sergejs Tiselskis <slokas85@gmail.con>
2022-11-22 07:38:53 +0200

    Initial Commit

---
+++ RealmCRUDTest/RealmCRUDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmCRUDTestApp.swift
+//  RealmCRUDTest
+//
+//  Created by John Doe on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RealmCRUDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
